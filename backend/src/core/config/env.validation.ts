c2ebd1ff951924ef1b71a827ea087f89641ef973
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2026-05-17 09:43:49 +0300

    add docker-compose.prod.yml and add cd

--- backend/src/core/config/env.validation.ts
+++ backend/src/core/config/env.validation.ts
@@ -7,7 +7,6 @@ export const envValidationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   ACCESS_TOKEN_SECRET: Joi.string().required(),
   ACCESS_TOKEN_TIME: Joi.number().required(),
-  REDIS_USE_TLS_AUTH: Joi.boolean().required(),
   REDIS_URL_AUTH: Joi.string().required(),
   ADMIN_USERNAME: Joi.string().required(),
   ADMIN_EMAIL: Joi.string().required(),
